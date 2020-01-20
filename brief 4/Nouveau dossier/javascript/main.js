93e77ea50f4883933f6eefeb5fc7692d92bbdafb
RachidOutahar <outahar.rac135@gmail.com>
2020-01-20 14:05:53 +0100

    le brief 4

--- brief 4/Nouveau dossier/javascript/main.js
+++ brief 4/Nouveau dossier/javascript/main.js
@@ -1,4 +1,6 @@
 const scrolToTopBtn = document.querySelector("#scrolToTopBtn");
+const fixTheContent = document.querySelector("#fexed-content");
+
 
 window.addEventListener("scroll", scrollFunction);
 function scrollFunction() {
@@ -16,7 +18,6 @@ function toTop() {
     window.scrollTo(0, 0);
 };
 
-
 window.addEventListener("scroll",function(){
     var off = window.pageYOffset;
     console.log(off)
@@ -25,10 +26,28 @@ window.addEventListener("scroll",function(){
 let navbar = document.getElementById("navbarscrol");
 
 window.onscroll = function() {
-    if (window.pageYOffset > 100) {
+    if (window.pageYOffset > 100 && window.innerWidth > 1100) {
         navbar.style.backgroundColor = "#494850";
     }
-    else{
+    else if(window.pageYOffset < 100 && window.innerWidth > 1100){
         navbar.style.backgroundColor = "transparent";
     }
+    if(window.pageYOffset > 724){
+        fixTheContent.classList.add("fexed-content");
+    }else if(window.pageYOffset < 724 || window.pageYOffset > 5037.33349609375){
+        fixTheContent.classList.remove("fexed-content");
+    }
 }
+window.addEventListener("resize",function(){
+    if(window.innerWidth < 1100){
+        navbar.style.backgroundColor = "#494850";
+    }
+    else if(window.pageYOffset < 100){
+        navbar.style.backgroundColor = "transparent";
+    }
+});
+window.addEventListener('scroll', function() {
+    if(window.pageYOffset > 5090){
+        fixTheContent.classList.remove("fexed-content");
+    }
+});
